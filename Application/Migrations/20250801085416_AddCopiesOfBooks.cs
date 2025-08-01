3e6dee6cc73dcc8de774c3cb7a4c2de0461f7c44
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-08-01 14:09:00 +0500

    chore: add migration

---
+++ Application/Migrations/20250801085416_AddCopiesOfBooks.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCopiesOfBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookCoverUrl",
+                table: "Books",
+                newName: "CoverUrl");
+
+            migrationBuilder.CreateTable(
+                name: "BooksCopies",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BookId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BooksCopies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BooksCopies_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BooksCopies_BookId",
+                table: "BooksCopies",
+                column: "BookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BooksCopies");
+
+            migrationBuilder.RenameColumn(
+                name: "CoverUrl",
+                table: "Books",
+                newName: "BookCoverUrl");
+        }
+    }
+}
