27577601bb6345ad42ddead0c80405858c6a420b
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-02-13 09:06:30 +0500

    feat: add architecture tag to knowledge areas

---
+++ Application/Migrations/20260213040557_AddArchitectureToKnowledgeAreas.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddArchitectureToKnowledgeAreas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "KnowledgeAreas",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 16L, "Architecture" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "KnowledgeAreas",
+                keyColumn: "Id",
+                keyValue: 16L);
+        }
+    }
+}
