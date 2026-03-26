35df0131c2733da2865047188362daba3cf5f790
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-03-26 16:20:15 +0500

    feat: #35: add addBookFeedback migration

---
+++ Application/Migrations/20260326105723_AddBookFeedback.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookFeedback",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TenantId = table.Column<long>(type: "bigint", nullable: false),
+                    BookId = table.Column<long>(type: "bigint", nullable: false),
+                    EmployeeId = table.Column<long>(type: "bigint", nullable: false),
+                    LeftFeedbackAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ProgressOfReading = table.Column<int>(type: "integer", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false),
+                    Advantages = table.Column<string>(type: "text", nullable: true),
+                    Disadvantages = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookFeedback", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookFeedback");
+        }
+    }
+}
