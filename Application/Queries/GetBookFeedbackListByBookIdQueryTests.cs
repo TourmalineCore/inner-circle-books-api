86af074cde5979b0efeec7cf4d000f8f0bc21532
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-03-27 13:42:21 +0500

    test: #35: add check for feedback for deleted book

--- Application/Queries/GetBookFeedbackListByBookIdQueryTests.cs
+++ Application/Queries/GetBookFeedbackListByBookIdQueryTests.cs
@@ -55,4 +55,41 @@ public class GetBookFeedbackListByBookIdQueryTests
 
     Assert.DoesNotContain(result, x => x.Id == bookFeedback.Id);
   }
+
+  [Fact]
+  public async Task GetFeedbackForDeletedBook_ShouldNotGetFeedbackForDeletedBook()
+  {
+    var book = new Book
+    {
+      Id = 1,
+      TenantId = TENANT_ID,
+      Title = "Test Book",
+      Annotation = "Test annotation",
+      Authors = new List<Author>()
+      {
+        new Author()
+        {
+          FullName = "Test Author"
+        }
+      },
+      Language = Language.en,
+      CoverUrl = "http://test-images.com/img404.png",
+      DeletedAtUtc = DateTime.UtcNow
+    };
+
+    var bookFeedback = new BookFeedback
+    {
+      Id = 1,
+      BookId = book.Id,
+      Book = book,
+      TenantId = TENANT_ID
+    };
+
+    await _context.BookFeedback.AddAsync(bookFeedback);
+    await _context.SaveChangesAsync();
+
+    var result = await _query.GetAsync(bookFeedback.BookId, TENANT_ID);
+
+    Assert.DoesNotContain(result, x => x.Id == bookFeedback.Id);
+  }
 }
